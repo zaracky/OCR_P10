5b595f6e5efa6cae6261cffccf931f8dfb4c0d18
zaracky <51820857+zaracky@users.noreply.github.com>
2020-02-13 13:38:49 -0800

    Fichier configuration auto
    hostname + droit

---
+++ configuration.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#hostname
+
+sudo hostname ubuntu
+sudo chmod o+w /etc/hostname
+sudo echo "ubuntu" > /etc/hostname
+sudo chmod o-w /etc/hostname
+
+
+#alias
+sudo echo "alias update='sudo apt-get update'" >> .bashrc
+sudo echo "alias upgrade='sudo apt-get upgrade'" >> .bashrc
+sudo echo "alias dc='sudo docker-compose'" >> .bashrc
+sudo echo "alias install='sudo apt-get install'" >> .bashrc
+
+source .bashrc
+
+#Modification droit
+sudo chmod o+w /etc/sudoers
+sudo echo "loic ALL=NOPASSWD: ALL" >> /etc/sudoers
+sudo chmod o-w /etc/sudoers
