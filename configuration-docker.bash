110d8aa9d5de7e8741180d008cae4c17a8bc3417
zaracky <51820857+zaracky@users.noreply.github.com>
2020-02-17 09:10:48 -0800

    Ajout -y

--- configuration-docker.bash
+++ configuration-docker.bash
@@ -11,10 +11,10 @@ source .bashrc
 
 #installation docker+compose
 
-sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose && sudo chmod +x /usr/bin/docker-compose
 
 #environnement docker
